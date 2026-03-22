f9a90548f7723e5d3a1d26a7002b875b1d149fa5
irissu73 <irissu73@gmail.com>
2026-03-22 20:41:31 +0800

    Update systemConfig.js

--- config/systemConfig.js
+++ config/systemConfig.js
@@ -1,5 +1,5 @@
 export const systemConfig = {
   requireEmail: false,     // Email 是否必填 true false
   maxExpireDays: 10,      // 最後保留日最大天數
-  sendEmailEnabled: true // 是否啟用寄信
+  sendEmailEnabled: false // 是否啟用寄信
 };
