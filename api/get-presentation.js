e0f82ab604e04d4ceb759b3276d5f57d39d7d7a9
irissu73 <irissu73@gmail.com>
2026-03-23 12:36:17 +0800

    Update get-presentation.js

--- api/get-presentation.js
+++ api/get-presentation.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json(data);
   } catch (err) {
-    console.error("get-presentation error:", err);
+    console.error("get-presentation error =", err);
     return res.status(500).json({ error: "讀取失敗" });
   }
 }
